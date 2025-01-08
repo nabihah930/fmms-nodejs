2cbf6bccd8b8615795899bbc640767ea5e7bf1c9
nabihah <nabihah.irfan@outlook.com>
2025-01-08 22:07:42 +0100

    Added Metabase data backup (windows)

---
+++ restore-metabase-data.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Restoring Metabase data volume...
+docker run --rm -v metabase-data:/metabase-data -v %cd%\data:/backup alpine sh -c "cd /metabase-data && tar -xzf /backup/metabase-data.tar.gz"
+echo Metabase data restored successfully!
